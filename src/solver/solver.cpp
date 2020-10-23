88d2f5854f748ccb9fc2f8f13f81d24b474b81c6
remi <remi.thibault.1@ens.etsmtl.ca>
2020-10-23 14:35:51 -0400

    rmv math.h

--- src/solver/solver.cpp
+++ src/solver/solver.cpp
@@ -9,24 +9,28 @@
 
 using std::memcpy;
 
-using std::this_thread::sleep_for;
 using std::chrono::microseconds;
+using std::this_thread::sleep_for;
 
-void solveSeq(int rows, int cols, int iterations, double td, double h, int sleep, double ** matrix) {
+void solveSeq(int rows, int cols, int iterations, double td, double h, int sleep, double **matrix)
+{
     double c, l, r, t, b;
-    
+
     double h_square = h * h;
 
-    double * linePrevBuffer = new double[cols];
-    double * lineCurrBuffer = new double[cols];
+    double *linePrevBuffer = new double[cols];
+    double *lineCurrBuffer = new double[cols];
 
-    for(int k = 0; k < iterations; k++) {
+    for (int k = 0; k < iterations; k++)
+    {
 
         memcpy(linePrevBuffer, matrix[0], cols * sizeof(double));
-        for(int i = 1; i < rows - 1; i++) {
+        for (int i = 1; i < rows - 1; i++)
+        {
 
             memcpy(lineCurrBuffer, matrix[i], cols * sizeof(double));
-            for(int j = 1; j < cols - 1; j++) {
+            for (int j = 1; j < cols - 1; j++)
+            {
                 c = lineCurrBuffer[j];
                 t = linePrevBuffer[j];
                 b = matrix[i + 1][j];
@@ -41,26 +45,34 @@ void solveSeq(int rows, int cols, int iterations, double td, double h, int sleep
     }
 }
 
-void solvePar(int rows, int cols, int iterations, double td, double h, int sleep, double ** matrix) {
+void solvePar(int rows, int cols, int iterations, double td, double h, int sleep, double **matrix)
+{
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    double c, l, r, t, b, q;
+
     double h_square = h * h;
 
-    for (int k=0; k<iterations; ++k)
+    for (int k = 0; k < iterations; ++k)
     {
-        for (int i=0; i<rows; ++i)
+        for (int i = 0; i < rows; ++i)
         {
-            for (int j=0; j<cols; ++j)
+            for (int j = 0; j < cols; ++j)
             {
-                matrix[i, j] = (1 - 4 * td / h_square) * matrix[i, j] + (td / h_square) * (matrix[i-1, j]+matrix[i+1, j]+matrix[i, j-1]+matrix[i, j+1]);
+                q = matrix[i, j];
+                l = matrix[i-1, j];
+                r = matrix[i+1, j];
+                t = matrix[i, j-1];
+                b = matrix[i, j+1];
+                matrix[i, j] = (1 - 4 * td / h_square) * q + (td / h_square) * (l + r + t + b);
                 sleep_for(microseconds(500000));
             }
         }
     }
 
-
-    if(0 != rank) {
+    if (0 != rank)
+    {
         deallocateMatrix(rows, matrix);
     }
 
